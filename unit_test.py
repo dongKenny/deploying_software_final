ea8e5fc2080fb11cc634c5dffe1c06cd4cdea240
dongKenny <kdong4@dons.usfca.edu>
2021-05-06 03:34:09 -0700

    Unit Tests and Requirements

--- unit_test.py
+++ unit_test.py
@@ -1,37 +1,41 @@
 import app
 import pytest
-import requests
 
 CLASS_API = "https://www.dnd5eapi.co/api/classes/"
 
 
 @pytest.fixture()
 def test_generate_response_failure():
-    params = {"format": "json"}
-    response = requests.get(CLASS_API + "Wizard", params=params)
-    return response
+    return app.generate_response("Wizard")
 
 
 def test_get_status_code_failure(test_generate_response_failure):
-    assert test_generate_response_failure.status_code != 200
+    assert app.get_status_code(test_generate_response_failure) != 200
 
 
 @pytest.fixture()
 def test_generate_response_success():
-    params = {"format": "json"}
-    response = requests.get(CLASS_API + "wizard", params=params)
-    return response
+    return app.generate_response("wizard")
 
 
 def test_get_status_code_success(test_generate_response_success):
-    assert test_generate_response_success.status_code == 200
+    assert app.get_status_code(test_generate_response_success) == 200
 
 
 def test_get_hit_die(test_generate_response_success):
-    assert test_generate_response_success.json()["hit_die"] == 6
+    assert app.get_hit_die(test_generate_response_success) == 6
 
 
 def test_get_saving_throw(test_generate_response_success):
+    assert app.get_saving_throw(test_generate_response_success) is not None
     firstThrow = test_generate_response_success.json()["saving_throws"][0]["index"]
     secondThrow = test_generate_response_success.json()["saving_throws"][1]["index"]
     assert firstThrow + ", " + secondThrow == "int, wis"
+
+
+def test_dnd_api_call():
+    expected = "API call made to: " + CLASS_API + "wizard\nResponse for api call was: 200\nHit die is: 6\nSaving " \
+                                                  "throws are: int, wis"
+    actual = app.dnd_api_call("wizard")
+    assert expected == actual
+
